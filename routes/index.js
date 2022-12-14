55bf3fd34a0bedaada781ea94716ef6708ecac7b
Juanan Pereira <juanan.pereira@ehu.eus>
2022-12-14 13:12:41 +0100

    Add Server Side Validation

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,72 @@
 var express = require('express');
 var router = express.Router();
 
-const { body, validationResult } = require('express-validator');
+const {body, validationResult} = require('express-validator');
 
 const mongojs = require('mongojs')
 const db = mongojs('mongodb://localhost:27017/usersdb', ['users'])
 
 function erabiltzaileakBistaratu(res) {
-  db.users.find(function (err, users) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('users', {title: 'Erabiltzaileak', users: users});
-    }
-  })
+    db.users.find(function (err, users) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('users', {title: 'Erabiltzaileak', users: users});
+        }
+    })
 }
 
-router.get('/', function(req, res, next) {
-  res.redirect('/users');
+router.get('/', function (req, res, next) {
+    res.redirect('/users');
 })
 
-router.get('/user', function(req, res, next) {
+router.get('/user', function (req, res, next) {
     res.redirect('/users');
 })
 
 /* GET home page. */
-router.get('/users', function(req, res, next) {
-  erabiltzaileakBistaratu(res)
+router.get('/users', function (req, res, next) {
+    erabiltzaileakBistaratu(res)
 });
 
 
-router.post('/user', function(req, res) {
-  const userBerria = {
-    izena : req.body.izena,
-    abizena: req.body.abizena,
-    email: req.body.email
-  };
+router.post('/user',
 
-  console.log(userBerria);
+    body('email').isEmail().withMessage('Email helbidea ez da zuzena'),
+    body('izena').isLength({min: 1}).withMessage('Izena ezin da hutsik egon'),
 
-  db.users.insert( userBerria, function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-        res.redirect('/users');
-    }
-  })
-})
+    function (req, res) {
 
-router.get('/user/deleteAll', function(req, res) {
-    db.users.remove({}, function(err) {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+
+        const userBerria = {
+            izena: req.body.izena,
+            abizena: req.body.abizena,
+            email: req.body.email
+        };
+
+        console.log(userBerria);
+
+        db.users.insert(userBerria, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect('/users');
+            }
+        })
+    })
+
+router.get('/user/deleteAll', function (req, res) {
+    db.users.remove({}, function (err) {
         if (err) {
-        console.log(err);
+            console.log(err);
         } else {
-          res.redirect('/users');
+            res.redirect('/users');
         }
     })
 })
